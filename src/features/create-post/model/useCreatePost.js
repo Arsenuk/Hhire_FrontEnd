184929f571feebc5afe80a8337423038bdef6986
Arsenuk <illya.arsenyuk@kpk-lp.com.ua>
2026-04-26 00:20:46 +0300

    move createpost to features

--- src/features/create-post/model/useCreatePost.js
+++ src/features/create-post/model/useCreatePost.js
@@ -56,6 +56,7 @@ export function useCreatePost () {
       for (const tag of tags) {
         formData.append('tags[]', tag)
       }
+
       for (const file of images.value) {
         formData.append('images', file)
       }
